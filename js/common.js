ec01d477998ac643075412166c781dcf8a67fce9
Dzzzg <756281300@qq.com>
2016-12-06 09:36:28 +0800

    Dzzzzg
    Dzzzzg

--- js/common.js
+++ js/common.js
@@ -16,3 +16,25 @@
 	$(document).ready(function() {
 		$().UItoTop({ easingType: 'easeOutQuart' });
 	});
+	//计时器
+	$(fuc=function(){
+	var t;
+	var d = new Date();
+	var hour = d.getHours();
+	var min = d.getMinutes();
+	var sec = d.getSeconds();
+	document.getElementById("bac").style.background="rgba("+(hour+Math.round(Math.random()*230))+","+(min+Math.round(Math.random()*230))+","+(sec+Math.round(Math.random()*230))+","+0.2+")";
+	document.getElementById("bac").style.color="rgb("+(hour+Math.round(Math.random()*230))+","+(min+Math.round(Math.random()*230))+","+(sec+Math.round(Math.random()*230))+")";
+	if (hour<10){
+		hour='0'+hour;
+	}
+	if (min<10){
+		min='0'+min;
+	}
+	if (sec<10){
+		sec='0'+sec;
+	}
+	document.getElementById("bac").innerHTML=hour+":"+min+":"+sec;
+	t=setTimeout("fuc()",1000);//调到1有奇效
+});
+	
